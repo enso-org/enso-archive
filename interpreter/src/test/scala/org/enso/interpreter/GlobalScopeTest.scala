382a407fd3675b5fd75e0d8b76ce3b165995ff0f
Ara Adkins <me@ara.io>
2019-07-08 16:41:55 +0100

    Rework one test

--- interpreter/src/test/scala/org/enso/interpreter/GlobalScopeTest.scala
+++ interpreter/src/test/scala/org/enso/interpreter/GlobalScopeTest.scala
@@ -3,11 +3,11 @@ package org.enso.interpreter
 class GlobalScopeTest extends LanguageTest {
   "Functions" should "use values from the global scope in their bodies" in {
     val code =
-    """
-      |a = 10;
-      |addTen = { |b| a + b };
-      |
-      |@addTen [5]
+      """
+        |a = 10;
+        |addTen = { |b| a + b };
+        |
+        |@addTen [5]
     """.stripMargin
 
     eval(code) shouldEqual 15
@@ -15,14 +15,14 @@ class GlobalScopeTest extends LanguageTest {
 
   "Functions" should "be able to call other functions in scope" in {
     val code =
-    """
-      |adder = { |a, b| a + b };
-      |
-      |@{ |multiply|
-      |  res = @adder [1, 2];
-      |  doubled = res * multiply;
-      |  doubled
-      |} [2]
+      """
+        |adder = { |a, b| a + b };
+        |
+        |@{ |multiply|
+        |  res = @adder [1, 2];
+        |  doubled = res * multiply;
+        |  doubled
+        |} [2]
     """.stripMargin
 
     eval(code) shouldEqual 6
@@ -31,15 +31,15 @@ class GlobalScopeTest extends LanguageTest {
   // A function is a first-class value
   "Functions" should "be able to be passed as values when in scope" in {
     val code =
-    """
-      |adder = { |a, b| a + b };
-      |
-      |binaryFn = { |a, b, function|
-      |  result = @function [a, b];
-      |  result
-      |}
-      |
-      |@binaryFn [1, 2, adder]
+      """
+        |adder = { |a, b| a + b };
+        |
+        |binaryFn = { |a, b, function|
+        |  result = @function [a, b];
+        |  result
+        |}
+        |
+        |@binaryFn [1, 2, adder]
     """.stripMargin
 
     eval(code) shouldEqual 3
@@ -48,19 +48,37 @@ class GlobalScopeTest extends LanguageTest {
   // Recursive calls between functions in the global scope
   "Functions" should "be able to mutually recurse in the global scope" in {
     val code =
-    """
-      |fn1 = { |number|
-      |  ifZero: [number % 3, number, @decrementCall [x]]
-      |}
-      |
-      |decrementCall = { |number|
-      |  res = number - 1;
-      |  @fn1 res
-      |
-      |@fn1 [5]
+      """
+        |fn1 = { |number|
+        |  ifZero: [number % 3, number, @decrementCall [x]]
+        |}
+        |
+        |decrementCall = { |number|
+        |  res = number - 1;
+        |  @fn1 res
+        |}
+        |
+        |@fn1 [5]
     """.stripMargin
 
     eval(code) shouldEqual 3
+  }
+
+  "Functions" should "be able to mutually recurse in the global scope" in {
+    val code =
+      """
+        |fn1 = { |number|
+        |  ifZero: [number % 3, number, @decrementCall [x]]
+        |}
+        |
+        |decrementCall = { |number|
+        |  res = number - 1;
+        |  @fn1 res
+        |
+        |fn1
+      """.stripMargin
+
+    eval(code).call(5) shouldEqual 3
   }
 
 }
