77e13c34596a39e9c96d5ce9145e3a49a1ee58f7
BinarySoftware <mkmikolajek@icloud.com>
2019-08-13 10:08:21 +0200

    Reformatting tests

--- syntax/specialization/src/test/scala/org/enso/syntax/text/DocParser.scala
+++ syntax/specialization/src/test/scala/org/enso/syntax/text/DocParser.scala
@@ -16,11 +16,12 @@ class DocParserSpec extends FlatSpec with Matchers {
     val output = DocParser.run(input)
     output match {
       case Result(_, Result.Success(value)) =>
-        logger.log(s"\nresult : '$result'")
-        logger.log(s"value  : '$value'")
+        logger.log("\nresult :")
+        pprint.pprintln(result, width = 50, height = 10000)
+        logger.log("\nvalue :")
+        pprint.pprintln(value, width = 50, height = 10000)
         logger.log(s"\ninput  : '$input'")
-        logger.log(s"v.show : '${value.show()}'\n")
-        logger.log(s"\nv.html : '${value.renderHTML()}'")
+        logger.log(s"\nv.show : '${value.show()}'\n")
         assert(value == result)
         assert(value.show() == input)
       case _ => fail(s"Parsing failed, consumed ${output.offset} chars")
@@ -32,35 +33,27 @@ class DocParserSpec extends FlatSpec with Matchers {
       val escape = (str: String) => str.replace("\n", "\\n")
       s"parse `${escape(str)}`"
     }
+
     private val testBase = it should parseTitle(input)
-    def ?=(out: Doc.AST): Unit = testBase in { assertExpr(input, out) }
+
+    def ?=(out: Doc.AST): Unit = testBase in {
+      assertExpr(input, out)
+    }
   }
+
+  //////////////////////////////////////////////////////////////////////////////
+  //////                  Tests of correct constructions                   /////
+  //////////////////////////////////////////////////////////////////////////////
+
   //////////////////////
   ///// Formatters /////
   //////////////////////
-  "*Foo*" ?= Doc(
-    Synopsis(
-      Section
-        .Raw(Formatter(Formatter.Bold, "Foo"))
-    )
-  )
-  "_Foo_" ?= Doc(
-    Synopsis(
-      Section.Raw(
-        Formatter(Formatter.Italic, "Foo")
-      )
-    )
-  )
+  "*Foo*" ?= Doc(Synopsis(Section.Raw(Formatter(Formatter.Bold, "Foo"))))
+  "_Foo_" ?= Doc(Synopsis(Section.Raw(Formatter(Formatter.Italic, "Foo"))))
   "~Foo~" ?= Doc(
-    Synopsis(
-      Section.Raw(
-        Formatter(Formatter.Strikethrough, "Foo")
-      )
-    )
+    Synopsis(Section.Raw(Formatter(Formatter.Strikethrough, "Foo")))
   )
-  "`Foo`" ?= Doc(
-    Synopsis(Section.Raw(Code.Inline("Foo")))
-  )
+  "`Foo`" ?= Doc(Synopsis(Section.Raw(Code.Inline("Foo"))))
   "~*Foo*~" ?= Doc(
     Synopsis(
       Section.Raw(
@@ -113,84 +106,12 @@ class DocParserSpec extends FlatSpec with Matchers {
       )
     )
   )
-  ///////////////////////////////
-  ///// Unclosed formatters /////
-  ///////////////////////////////
-  "_*Foo*" ?= Doc(
-    Synopsis(
-      Section.Raw(
-        Formatter.Unclosed(Formatter.Italic, Formatter(Formatter.Bold, "Foo"))
-      )
-    )
-  )
-  "~*Foo*" ?= Doc(
-    Synopsis(
-      Section.Raw(
-        Formatter
-          .Unclosed(Formatter.Strikethrough, Formatter(Formatter.Bold, "Foo"))
-      )
-    )
-  )
-  "***Foo" ?= Doc(
-    Synopsis(
-      Section.Raw(
-        Formatter(Formatter.Bold),
-        Formatter.Unclosed(Formatter.Bold, "Foo")
-      )
-    )
-  )
-  "*_Foo_" ?= Doc(
-    Synopsis(
-      Section.Raw(
-        Formatter.Unclosed(Formatter.Bold, Formatter(Formatter.Italic, "Foo"))
-      )
-    )
-  )
-  "~_Foo_" ?= Doc(
-    Synopsis(
-      Section.Raw(
-        Formatter
-          .Unclosed(Formatter.Strikethrough, Formatter(Formatter.Italic, "Foo"))
-      )
-    )
-  )
-  "___Foo" ?= Doc(
-    Synopsis(
-      Section.Raw(
-        Formatter(Formatter.Italic),
-        Formatter.Unclosed(Formatter.Italic, "Foo")
-      )
-    )
-  )
-  "*~Foo~" ?= Doc(
-    Synopsis(
-      Section.Raw(
-        Formatter
-          .Unclosed(Formatter.Bold, Formatter(Formatter.Strikethrough, "Foo"))
-      )
-    )
-  )
-  "_~Foo~" ?= Doc(
-    Synopsis(
-      Section.Raw(
-        Formatter
-          .Unclosed(Formatter.Italic, Formatter(Formatter.Strikethrough, "Foo"))
-      )
-    )
-  )
-  "~~~Foo" ?= Doc(
-    Synopsis(
-      Section.Raw(
-        Formatter(Formatter.Strikethrough),
-        Formatter.Unclosed(Formatter.Strikethrough, "Foo")
-      )
-    )
-  )
   "`import foo`" ?= Doc(
     Synopsis(
       Section.Raw(Code.Inline("import foo"))
     )
   )
+
   ////////////////////
   ///// Segments /////
   ////////////////////
@@ -227,6 +148,7 @@ class DocParserSpec extends FlatSpec with Matchers {
       Section.Marked(Section.Marked.Example, Section.Header("Example"))
     )
   )
+
   /////////////////
   ///// Lists /////
   /////////////////
@@ -316,6 +238,173 @@ class DocParserSpec extends FlatSpec with Matchers {
       )
     )
   )
+
+  /////////////////
+  ///// Links /////
+  /////////////////
+  "[Hello](Http://Google.com)" ?= Doc(
+    Synopsis(
+      Section.Raw(
+        Link.URL(
+          "Hello",
+          "Http://Google.com"
+        )
+      )
+    )
+  )
+  "![Media](http://foo.com)" ?= Doc(
+    Synopsis(
+      Section.Raw(
+        Link.Image(
+          "Media",
+          "http://foo.com"
+        )
+      )
+    )
+  )
+
+  ////////////////
+  ///// Tags /////
+  ////////////////
+  "DEPRECATED\n" ?= Doc(Tags(Tags.Tag(Tags.Tag.Deprecated)))
+  "DEPRECATED\nFoo" ?= Doc(
+    Tags(Tags.Tag(Tags.Tag.Deprecated)),
+    Synopsis(Section.Raw("Foo"))
+  )
+  "MODIFIED\nFoo" ?= Doc(
+    Tags(Tags.Tag(Tags.Tag.Modified)),
+    Synopsis(Section.Raw("Foo"))
+  )
+  "ADDED\nFoo" ?= Doc(
+    Tags(Tags.Tag(Tags.Tag.Added)),
+    Synopsis(Section.Raw("Foo"))
+  )
+  "REMOVED\nFoo" ?= Doc(
+    Tags(Tags.Tag(Tags.Tag.Removed)),
+    Synopsis(Section.Raw("Foo"))
+  )
+  "DEPRECATED in 1.0\nFoo" ?= Doc(
+    Tags(Tags.Tag(Tags.Tag.Deprecated, " in 1.0")),
+    Synopsis(Section.Raw("Foo"))
+  )
+  "DEPRECATED in 1.0\nMODIFIED\nFoo" ?= Doc(
+    Tags(Tags.Tag(Tags.Tag.Deprecated, " in 1.0"), Tags.Tag(Tags.Tag.Modified)),
+    Synopsis(Section.Raw("Foo"))
+  )
+
+  /////////////////
+  ///// other /////
+  /////////////////
+  "Foo *Foo* ~*Bar~ `foo bar baz bo` \n\n\nHello Section\n\n!important\n\n?Hi\n\n>Example" ?= Doc(
+    Synopsis(
+      Section.Raw(
+        Line(
+          "Foo ",
+          Formatter(Formatter.Bold, "Foo"),
+          " ",
+          Formatter(
+            Formatter.Strikethrough,
+            Formatter.Unclosed(Formatter.Bold, "Bar")
+          ),
+          " ",
+          Code.Inline("foo bar baz bo"),
+          " "
+        )
+      )
+    ),
+    Body(
+      Section.Raw(Section.Header(Line("Hello Section"))),
+      Section
+        .Marked(
+          Section.Marked.Important,
+          Section.Header(Line("important"))
+        ),
+      Section.Marked(Section.Marked.Info, Section.Header(Line("Hi"))),
+      Section.Marked(Section.Marked.Example, Section.Header("Example"))
+    )
+  )
+
+  //////////////////////////////////////////////////////////////////////////////
+  //////                Tests of incorrect constructions                   /////
+  //////////////////////////////////////////////////////////////////////////////
+
+  ///////////////////////////////
+  ///// Unclosed formatters /////
+  ///////////////////////////////
+  "_*Foo*" ?= Doc(
+    Synopsis(
+      Section.Raw(
+        Formatter.Unclosed(Formatter.Italic, Formatter(Formatter.Bold, "Foo"))
+      )
+    )
+  )
+  "~*Foo*" ?= Doc(
+    Synopsis(
+      Section.Raw(
+        Formatter
+          .Unclosed(Formatter.Strikethrough, Formatter(Formatter.Bold, "Foo"))
+      )
+    )
+  )
+  "***Foo" ?= Doc(
+    Synopsis(
+      Section.Raw(
+        Formatter(Formatter.Bold),
+        Formatter.Unclosed(Formatter.Bold, "Foo")
+      )
+    )
+  )
+  "*_Foo_" ?= Doc(
+    Synopsis(
+      Section.Raw(
+        Formatter.Unclosed(Formatter.Bold, Formatter(Formatter.Italic, "Foo"))
+      )
+    )
+  )
+  "~_Foo_" ?= Doc(
+    Synopsis(
+      Section.Raw(
+        Formatter
+          .Unclosed(Formatter.Strikethrough, Formatter(Formatter.Italic, "Foo"))
+      )
+    )
+  )
+  "___Foo" ?= Doc(
+    Synopsis(
+      Section.Raw(
+        Formatter(Formatter.Italic),
+        Formatter.Unclosed(Formatter.Italic, "Foo")
+      )
+    )
+  )
+  "*~Foo~" ?= Doc(
+    Synopsis(
+      Section.Raw(
+        Formatter
+          .Unclosed(Formatter.Bold, Formatter(Formatter.Strikethrough, "Foo"))
+      )
+    )
+  )
+  "_~Foo~" ?= Doc(
+    Synopsis(
+      Section.Raw(
+        Formatter
+          .Unclosed(Formatter.Italic, Formatter(Formatter.Strikethrough, "Foo"))
+      )
+    )
+  )
+  "~~~Foo" ?= Doc(
+    Synopsis(
+      Section.Raw(
+        Formatter(Formatter.Strikethrough),
+        Formatter.Unclosed(Formatter.Strikethrough, "Foo")
+      )
+    )
+  )
+
+  ////////////////////
+  /// Wrong indent ///
+  ////////////////////
   """List
     |  - First unordered item
     |  - Second unordered item
@@ -363,88 +452,58 @@ class DocParserSpec extends FlatSpec with Matchers {
       )
     )
   )
-  /////////////////
-  ///// Links /////
-  /////////////////
-  "[Hello](Http://Google.com)" ?= Doc(
-    Synopsis(
-      Section.Raw(
-        Link.URL(
-          "Hello",
-          "Http://Google.com"
-        )
-      )
-    )
-  )
-  "![Media](http://foo.com)" ?= Doc(
-    Synopsis(
-      Section.Raw(
-        Link.Image(
-          "Media",
-          "http://foo.com"
-        )
-      )
-    )
-  )
-  /////////////////
-  ///// other /////
-  /////////////////
-  "Foo *Foo* ~*Bar~ `foo bar baz bo` \n\n\nHello Section\n\n!important\n\n?Hi\n\n>Example" ?= Doc(
-    Synopsis(
-      Section.Raw(
-        Line(
-          "Foo ",
-          Formatter(Formatter.Bold, "Foo"),
-          " ",
-          Formatter(
-            Formatter.Strikethrough,
-            Formatter.Unclosed(Formatter.Bold, "Bar")
-          ),
-          " ",
-          Code.Inline("foo bar baz bo"),
-          " "
-        )
-      )
-    ),
-    Body(
-      Section.Raw(Section.Header(Line("Hello Section"))),
-      Section
-        .Marked(
-          Section.Marked.Important,
-          Section.Header(Line("important"))
-        ),
-      Section.Marked(Section.Marked.Info, Section.Header(Line("Hi"))),
-      Section.Marked(Section.Marked.Example, Section.Header("Example"))
-    )
-  )
-  ////////////////
-  ///// Tags /////
-  ////////////////
-  "DEPRECATED\n" ?= Doc(
-    Tags(Tags.Tag(Tags.Tag.Deprecated))
-  )
-  "DEPRECATED\nFoo" ?= Doc(
-    Tags(Tags.Tag(Tags.Tag.Deprecated)),
-    Synopsis(Section.Raw("Foo"))
-  )
-  "MODIFIED\nFoo" ?= Doc(
-    Tags(Tags.Tag(Tags.Tag.Modified)),
-    Synopsis(Section.Raw("Foo"))
-  )
-  "ADDED\nFoo" ?= Doc(
-    Tags(Tags.Tag(Tags.Tag.Added)),
-    Synopsis(Section.Raw("Foo"))
-  )
-  "REMOVED\nFoo" ?= Doc(
-    Tags(Tags.Tag(Tags.Tag.Removed)),
-    Synopsis(Section.Raw("Foo"))
-  )
-  "DEPRECATED in 1.0\nFoo" ?= Doc(
-    Tags(Tags.Tag(Tags.Tag.Deprecated, " in 1.0")),
-    Synopsis(Section.Raw("Foo"))
-  )
-  "DEPRECATED in 1.0\nMODIFIED\nFoo" ?= Doc(
-    Tags(Tags.Tag(Tags.Tag.Deprecated, " in 1.0"), Tags.Tag(Tags.Tag.Modified)),
-    Synopsis(Section.Raw("Foo"))
-  )
+
+  """   ALAMAKOTA a kot ma ale
+    | foo bar
+    |""".stripMargin
+
+  """
+    | - bar
+    | baz
+    |""".stripMargin
+
+  """   DEPRECATED das sfa asf
+    |REMOVED fdsdf
+    |Construct and manage a graphical, event-driven user interface for your iOS or
+    |tvOS app.
+    |
+    | br *foo
+    | dsad*""".stripMargin
+
+  """   DEPRECATED das sfa asf
+    |REMOVED fdsdf
+    |Construct and manage a graphical, event-driven user interface for your iOS or
+    |tvOS app.
+    |
+    | *foo*""".stripMargin
+
+  """   DEPRECATED das sfa asf
+    |REMOVED fdsdf
+    |Construct and manage a graphical, event-driven user interface for your iOS or
+    |tvOS app.
+    |
+    | *foo
+    | *foo2""".stripMargin
+
+  """   DEPRECATED das sfa asf
+    |REMOVED fdsdf
+    |Construct and manage a graphical, event-driven user interface for your iOS or
+    |tvOS app.
+    |
+    | *foo """.stripMargin
+
+  """   DEPRECATED das sfa asf
+    |REMOVED fdsdf
+    |Construct and manage a graphical, event-driven user interface for your iOS or
+    |tvOS app.
+    |
+    |   fooo bar baz
+    |   dsadasfsaf asfasfas
+    |   fasfasfa sf
+    |   asfas fasf """.stripMargin
+
+  """    DEPRECATED das sfa asf
+    |  REMOVED
+    |""".stripMargin
+
 }
