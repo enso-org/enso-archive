965518cd499488c1645eb6d3dcef5152b70566fe
Marcin Kostrzewa <marckostrzewa@gmail.com>
2019-08-13 18:22:10 +0200

    moar doc

--- Interpreter/src/main/java/org/enso/interpreter/runtime/error/NoMethodErrorException.java
+++ Interpreter/src/main/java/org/enso/interpreter/runtime/error/NoMethodErrorException.java
@@ -3,27 +3,28 @@ package org.enso.interpreter.runtime.error;
 import com.oracle.truffle.api.TruffleException;
 import com.oracle.truffle.api.nodes.Node;
 
+/** Thrown when a given Object does not implement the requested Enso-level method. */
 public class NoMethodErrorException extends RuntimeException implements TruffleException {
-    private final Node node;
+  private final Node node;
 
-    /**
-     * Creates the error.
-     *
-     * @param target the construct that was attempted to be invoked
-     * @param node the node where the erroneous invocation took place
-     */
-    public NoMethodErrorException(Object target, String name, Node node) {
-        super("Object " + target + " does not define method " + name + ".");
-        this.node = node;
-    }
+  /**
+   * Creates the error.
+   *
+   * @param target the construct that was attempted to be invoked
+   * @param node the node where the erroneous invocation took place
+   */
+  public NoMethodErrorException(Object target, String name, Node node) {
+    super("Object " + target + " does not define method " + name + ".");
+    this.node = node;
+  }
 
-    /**
-     * The location where the erroneous invocation took place.
-     *
-     * @return the node where the error occurred
-     */
-    @Override
-    public Node getLocation() {
-        return node;
-    }
+  /**
+   * The location where the erroneous invocation took place.
+   *
+   * @return the node where the error occurred
+   */
+  @Override
+  public Node getLocation() {
+    return node;
+  }
 }
