199a1774b64166b8ea8caae74f39f0e6e443104e
BinarySoftware <mkmikolajek@icloud.com>
2019-10-17 14:32:04 +0200

    Change example text

--- Syntax/specialization/src/main/scala/org/enso/syntax/text/Parser.scala
+++ Syntax/specialization/src/main/scala/org/enso/syntax/text/Parser.scala
@@ -217,7 +217,7 @@ class Parser {
 
 object Parser {
   type IDMap = Seq[((Int, Int), AST.ID)]
-  def apply(): Parser   = new Parser()
+  def apply(): Parser = new Parser()
   private val newEngine = flexer.Parser.compile(ParserDef())
 
   //// Exceptions ////
@@ -311,34 +311,37 @@ object Main extends App {
     """
       |##
       |  DEPRECATED
-      |  REMOVED - replaced by SwiftUI
+      |  REMOVED - replaced by Foo Bar
       |  ADDED
       |  MODIFIED
       |  UPCOMING
       |  ALAMAKOTA a kot ma Ale
-      |  Construct and manage a graphical, event-driven user interface for your
-      |  iOS or tvOS app.
+      |  This is a test of Enso Documentation Parser. This is a short synopsis.
       | 
-      |  The UIKit framework provides the required infrastructure for your iOS or
-      |  tvOS apps. It provides the window and view architecture for implementing
-      |  your interface, the event handling infrastructure for delivering Multi-
-      |  Touch and other types of input to your app, and the main run loop needed
-      |  to manage interactions among the user, the system, and your app. Other
-      |  features offered by the framework include animation support, document
-      |  support, drawing and printing support, information about the current
-      |  device, text management and display, search support, accessibility  
-      |  support, app extension support, and resource management.
+      |  Here you can write the body of documentation. On top you can see tags
+      |  added to this piece of code. You can customise your text with _Italic_
+      |  ~Strikethrough~ or *Bold*. ~_*Combined*_~ is funny
+      |  
+      |  
+      |  There are 3 kinds of sections
+      |    - Important
+      |    - Info
+      |    - Example
+      |      * You can use example to add multiline code to your documentation
       | 
       |  ! Important
-      |    Use UIKit classes only from your app’s main thread or main dispatch
-      |    queue, unless otherwise indicated. This restriction particularly
-      |    applies to classes derived from UIResponder or that involve
-      |    manipulating your app’s user interface in any way.
+      |    Here is a small test of Important Section
+      |    
+      |  ? Info
+      |    Here is a small test of Info Section
+      |    
+      |  > Example
+      |    Here is a small test of Example Section
+      |        Import Foo
+      |        def Bar a
       |def Maybe a
       |    ## test attached to Just
       |    def Just val:a
-      |    ## this function subtracts numbers
-      |    sub x y = x - y
       |    def Nothing
       |""".stripMargin
 
