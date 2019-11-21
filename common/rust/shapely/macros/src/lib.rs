194c6736ea0f7bc8b362aff2062ac265178bf0be
mwu <mwu-tow@gazeta.pl>
2019-11-21 14:22:06 +0100

    doc

--- common/rust/shapely/macros/src/lib.rs
+++ common/rust/shapely/macros/src/lib.rs
@@ -1,18 +1,7 @@
-//! This module defines a custom derive macro `Iterator`.
-//!
-//! The `#[derive(Iterator)]` can be used on any struct type that takes at least
-//! a single type parameter. For `struct Foo` it will provide:
-//! * `IntoIterator` implementations for `&'t Foo<T>` and `&mut 't Foo<T>`
-//! * `iter` and `into_iter` methods
-//!
-//! The iterators will go over each field that declared type is same as the
-//! struct's last type parameter.
-//!
-//! Caller must have the following features enabled:
-//! * `#![feature(generators)]`
-//! * `#![feature(type_alias_impl_trait)]`
-//!
-//!
+//! This crate defines a custom derive macro `Iterator`. Should not be used
+//! directly, but only through `shapely` crate, as it provides utilities
+//! necessary for the generated code to compile.
+
 extern crate proc_macro;
 
 use prelude::*;
@@ -25,8 +14,21 @@ use syn;
 
 ////////////////////////////////////////////////
 
-/// Macro entry point, decides on which type parameter will be used to detect
-/// iterable fields.
+/// For `struct Foo<T>` provides:
+/// * `IntoIterator` implementations for `&'t Foo<T>` and `&mut 't Foo<T>`;
+/// * `iter` and `into_iter` methods.
+///
+/// The iterators will go over each field that declared type is same as the
+/// struct's last type parameter.
+///
+/// Caller must have the following features enabled:
+/// ```
+/// #![feature(generators)]
+/// #![feature(type_alias_impl_trait)]
+/// ```
+///
+/// When used on type that takes no type parameters, like `struct Foo`, does
+/// nothing but yields no errors.
 #[proc_macro_derive(Iterator)]
 pub fn derive_iterator
 (input: proc_macro::TokenStream) -> proc_macro::TokenStream {
