2f54b26f9b63b5d56a7b24a089b8f6d5ad05e691
Marcin Kostrzewa <marckostrzewa@gmail.com>
2019-12-02 19:12:08 +0100

    CR feedback

--- engine/runtime/src/test/java/org/enso/interpreter/test/CodeLocationsTestInstrument.java
+++ engine/runtime/src/test/java/org/enso/interpreter/test/CodeLocationsTestInstrument.java
@@ -5,6 +5,12 @@ import com.oracle.truffle.api.instrumentation.*;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.source.SourceSection;
 
+/**
+ * A debug instrument used to test code locations.
+ *
+ * <p>Allows to listen for a node with a given type at a given position, and later verify if such a
+ * node was indeed encountered in the course of execution.
+ */
 @TruffleInstrument.Registration(
     id = CodeLocationsTestInstrument.INSTRUMENT_ID,
     services = CodeLocationsTestInstrument.class)
@@ -12,40 +18,76 @@ public class CodeLocationsTestInstrument extends TruffleInstrument {
   public static final String INSTRUMENT_ID = "locations-test";
   private Env env;
 
+  /**
+   * Initializes the instrument. Substitute for a constructor, called by the Truffle framework.
+   *
+   * @param env the instrumentation environment
+   */
   @Override
   protected void onCreate(Env env) {
     env.registerService(this);
     this.env = env;
   }
 
+  /**
+   * An event listener implementing the behavior of verifying whether the currently executed node is
+   * the one expected by the user.
+   */
   public static class LocationsEventListener implements ExecutionEventListener {
     private boolean successful = false;
     private final int start;
     private final int length;
     private final Class<?> type;
 
-    public LocationsEventListener(int start, int length, Class<?> type) {
+    private LocationsEventListener(int start, int length, Class<?> type) {
       this.start = start;
       this.length = length;
       this.type = type;
     }
 
+    /**
+     * Get the start location of the nodes expected by this listener.
+     *
+     * @return the start location for this listener
+     */
     public int getStart() {
       return start;
     }
 
+    /**
+     * Get the source length of the nodes expected by this listener.
+     *
+     * @return the source length for this listener
+     */
     public int getLength() {
       return length;
     }
 
+    /**
+     * Get the type of nodes expected by this listener.
+     *
+     * @return the node type for this listener
+     */
     public Class<?> getType() {
       return type;
     }
 
+    /**
+     * Was a node with parameters specified for this listener encountered in the course of
+     * execution?
+     *
+     * @return {@code true} if the requested node was observed, {@code false} otherwise
+     */
     public boolean isSuccessful() {
       return successful;
     }
 
+    /**
+     * Checks if the node to be executed is the node this listener was created to observe.
+     *
+     * @param context current execution context
+     * @param frame current execution frame
+     */
     @Override
     public void onEnter(EventContext context, VirtualFrame frame) {
       if (successful) {
@@ -72,6 +114,14 @@ public class CodeLocationsTestInstrument extends TruffleInstrument {
         EventContext context, VirtualFrame frame, Throwable exception) {}
   }
 
+  /**
+   * Attach a new listener to observe nodes with given parameters.
+   *
+   * @param sourceStart the source start location of the expected node
+   * @param length the source length of the expected node
+   * @param type the type of the expected node
+   * @return a reference to attached event listener
+   */
   public EventBinding<LocationsEventListener> bindTo(int sourceStart, int length, Class<?> type) {
     return env.getInstrumenter()
         .attachExecutionEventListener(
