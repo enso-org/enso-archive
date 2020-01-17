56b28a727ffabae4ffab51d98066c1200bd75bb2
mwu <mwu-tow@gazeta.pl>
2020-01-17 14:39:06 +0100

    file manager stub

---
+++ common/rust/file-manager-client/src/lib.rs
@@ -0,0 +1,101 @@
+use prelude::*;
+
+use serde::Serialize;
+use serde::Deserialize;
+use json_rpc::*;
+use json_rpc::api::Result;
+use std::future::Future;
+use futures::FutureExt;
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+pub mod web_transport;
+pub mod web_main;
+
+pub type Path = String;
+
+#[derive(Debug)]
+pub struct FmClient {
+    pub handler:Handler,
+}
+
+impl FmClient {
+    pub fn new(transport:impl Transport + 'static) -> FmClient {
+        let mut handler = Handler::new(transport);
+        handler.on_error.set(|e| {
+            log!("Encountered handling error: {:?}", e);
+        });
+        handler.on_notification.set(|n| {
+            log!("Received a notification: {:?}", n);
+        });
+        FmClient { handler }
+    }
+
+    pub fn copy_file
+    (&mut self, from:Path, to:Path) -> impl Future<Output = Result<()>> {
+        let input = CopyFileInput { from, to };
+        self.handler.open_request(input).map(|result| result.map(|_| ()))
+    }
+    pub fn exists(&mut self, path:Path) -> impl Future<Output = Result<bool>> {
+        println!("exists?");
+        let input = ExistsInput { path };
+        self.handler.open_request(input).map(|result| result.map(|r| r.exists))
+    }
+    pub fn touch(&mut self, path:Path) -> impl Future<Output = Result<()>> {
+        println!("exists?");
+        let input = ExistsInput { path };
+        self.handler.open_request(input).map(|result| result.map(|_| ()))
+    }
+
+    pub fn tick(&mut self) {
+        self.handler.process_events()
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct CopyFileInput {
+    from : String,
+    to   : String,
+}
+impl api::RemoteMethodCall for CopyFileInput {
+    const NAME:&'static str = "copy";
+    type Returned = CopyFileResponse;
+}
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct CopyFileResponse {}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct ExistsInput {
+    path : String
+}
+
+impl api::RemoteMethodCall for ExistsInput {
+    const NAME:&'static str = "exists";
+    type Returned = ExistsResponse;
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct ExistsResponse {
+    exists : bool
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct TouchInput {
+    path : String
+}
+
+impl api::RemoteMethodCall for TouchInput {
+    const NAME:&'static str = "Touch";
+    type Returned = TouchResponse;
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct TouchResponse {
+    exists : bool
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct FileSystemEventInput {}
---
+++ common/rust/file-manager-client/src/web_transport.rs
@@ -0,0 +1,93 @@
+use prelude::*;
+
+use crate::log;
+
+use js_sys::Function;
+use json_rpc::Transport;
+use json_rpc::TransportCallbacks;
+
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+//use web_sys::Blob;
+use web_sys::CloseEvent;
+//use web_sys::ErrorEvent;
+use web_sys::MessageEvent;
+use web_sys::WebSocket;
+
+#[derive(Debug)]
+pub struct MyWebSocket {
+    pub ws                 : web_sys::WebSocket,
+    pub on_message_closure : ClosureStorage<MessageEvent>,
+    pub on_closed_closure  : ClosureStorage<CloseEvent>,
+}
+
+#[derive(Debug)]
+pub struct ClosureStorage<T> {
+    pub closure:Option<Closure<dyn FnMut(T)>>,
+}
+impl <T> ClosureStorage<T> {
+    pub fn new() -> ClosureStorage<T> {
+        ClosureStorage { closure : None }
+    }
+    pub fn store(&mut self, closure:Closure<dyn FnMut(T)>) {
+        self.closure = Some(closure);
+    }
+    pub fn function(&self) -> Option<&Function> {
+        self.closure.as_ref().map(|closure| closure.as_ref().unchecked_ref() )
+    }
+}
+impl MyWebSocket {
+    pub async fn new(url:&str) -> MyWebSocket {
+        MyWebSocket {
+            ws                 : new_websocket(url).await,
+            on_message_closure : ClosureStorage::new(),
+            on_closed_closure  : ClosureStorage::new(),
+        }
+    }
+}
+
+impl Transport for MyWebSocket {
+    fn send_text(&mut self, message:String) {
+        log!("will send text message: {}", message);
+        self.ws.send_with_str(&message);
+    }
+
+    fn set_callback(&mut self, cb:Rc<RefCell<dyn TransportCallbacks>>) {
+        let cb1 = cb.clone();
+        let on_message = Closure::wrap(Box::new(move |e: MessageEvent| {
+            let data = e.data();
+            if let Some(text) = data.as_string() {
+                log!("received text message: {}", text);
+                cb1.borrow_mut().on_text_message(text);
+            }
+        }) as Box<dyn FnMut(MessageEvent)>);
+        self.on_message_closure.store(on_message);
+        self.ws.set_onmessage(self.on_message_closure.function());
+
+        let on_close = Closure::wrap(Box::new(move |e:CloseEvent| {
+            log!("socket closed {:?}", e);
+            cb.borrow_mut().on_close();
+        }) as Box<dyn FnMut(CloseEvent)>);
+
+        self.on_closed_closure.store(on_close);
+        self.ws.set_onclose(self.on_closed_closure.function());
+    }
+}
+
+pub async fn new_websocket(url:&str) -> WebSocket {
+    log!("Starting new WebSocket connecting to {}...", url);
+    let (sender, receiver) = futures::channel::oneshot::channel::<WebSocket>();
+    let ws = WebSocket::new(url).unwrap();
+    let cloned_ws = ws.clone();
+    let sender = Rc::new(RefCell::new(Some(sender)));
+    let onopen_callback = Closure::wrap(Box::new(move |_| {
+        if let Some(s) = sender.borrow_mut().take() {
+            log!("WebSocket successfully opened!");
+            s.send(cloned_ws.clone());
+            cloned_ws.set_onopen(None);
+        }
+    }) as Box<dyn FnMut(JsValue)>);
+    ws.set_onopen(Some(onopen_callback.as_ref().unchecked_ref()));
+    onopen_callback.forget();
+    receiver.await.unwrap()
+}
