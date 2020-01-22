1833f356b192526b506d0c235a7773c8fbd41512
DmytroMitin <dmitin3@gmail.com>
2020-01-22 18:05:02 +0200

    Clean up

--- engine/gateway/src/main/scala/org/enso/gateway/protocol/response/result/servercapabilities/textdocumentsync/TextDocumentSyncKind.scala
+++ engine/gateway/src/main/scala/org/enso/gateway/protocol/response/result/servercapabilities/textdocumentsync/TextDocumentSyncKind.scala
@@ -12,14 +12,6 @@ object TextDocumentSyncKind {
 
   object Incremental extends TextDocumentSyncKind(2)
 
-  //  implicit val textDocumentSyncKindDecoder: Decoder[TextDocumentSyncKind] =
-  //    Decoder.decodeInt.emap {
-  //      case 0 => Right(NoneKind)
-  //      case 1 => Right(Full)
-  //      case 2 => Right(Incremental)
-  //      case _ => Left("Invalid TextDocumentSyncKind")
-  //    }
-
   implicit val textDocumentSyncKindEncoder: Encoder[TextDocumentSyncKind] =
     Encoder.encodeInt.contramap(_.value)
 }
