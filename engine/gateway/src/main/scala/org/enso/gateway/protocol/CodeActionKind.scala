6e5bc987de3ab5425adcef914b0bc834f7888d63
DmytroMitin <dmitin3@gmail.com>
2020-01-23 13:35:31 +0200

    Add docs

--- engine/gateway/src/main/scala/org/enso/gateway/protocol/CodeActionKind.scala
+++ engine/gateway/src/main/scala/org/enso/gateway/protocol/CodeActionKind.scala
@@ -2,6 +2,12 @@ package org.enso.gateway.protocol
 
 import io.circe.{Decoder, Encoder}
 
+/** Kind of
+  * [[org.enso.gateway.protocol.request.clientcapabilities.textdocument.CodeAction]].
+  *
+  * Used also in
+  * [[org.enso.gateway.protocol.response.result.servercapabilities.CodeActionProvider]].
+  */
 sealed abstract class CodeActionKind(val value: String)
 object CodeActionKind {
   private val empty                 = ""
@@ -14,20 +20,30 @@ object CodeActionKind {
   private val sourceOrganizeImports = "source.organizeImports"
   private val invalidCodeActionKind = "Invalid CodeActionKind"
 
+  /** Empty kind. */
   case object Empty extends CodeActionKind(empty)
 
+  /** Base kind for quickfix actions. */
   case object QuickFix extends CodeActionKind(quickfix)
 
+  /** Base kind for refactoring actions. */
   case object Refactor extends CodeActionKind(refactor)
 
+  /** Base kind for refactoring extraction actions. */
   case object RefactorExtract extends CodeActionKind(refactorExtract)
 
+  /** Base kind for refactoring inline actions. */
   case object RefactorInline extends CodeActionKind(refactorInline)
 
+  /** Base kind for refactoring rewrite actions. */
   case object RefactorRewrite extends CodeActionKind(refactorRewrite)
 
-  case object CodeActionKind extends CodeActionKind(source)
+  /** Base kind for source actions. Source code actions apply to the entire
+    * file.
+    */
+  case object Source extends CodeActionKind(source)
 
+  /** Base kind for an organize imports source action. */
   case object SourceOrganizeImports
       extends CodeActionKind(sourceOrganizeImports)
 
@@ -39,7 +55,7 @@ object CodeActionKind {
       case `refactorExtract`       => Right(RefactorExtract)
       case `refactorInline`        => Right(RefactorInline)
       case `refactorRewrite`       => Right(RefactorRewrite)
-      case `source`                => Right(CodeActionKind)
+      case `source`                => Right(Source)
       case `sourceOrganizeImports` => Right(SourceOrganizeImports)
       case _                       => Left(invalidCodeActionKind)
     }
