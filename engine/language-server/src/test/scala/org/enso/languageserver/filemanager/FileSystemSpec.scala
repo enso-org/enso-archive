05714d10ff3aad1d7f578368889bcacd6493f3df
lolczak <lukasz.jan.olczak@gmail.com>
2020-02-21 21:32:44 +0100

    overwriting functionality

--- engine/language-server/src/test/scala/org/enso/languageserver/filemanager/FileSystemSpec.scala
+++ engine/language-server/src/test/scala/org/enso/languageserver/filemanager/FileSystemSpec.scala
@@ -14,12 +14,23 @@ class FileSystemSpec extends AnyFlatSpec with Matchers {
     val path    = Paths.get(TestDirPath.toString, "foo.txt")
     val content = "123456789"
     //when
-    println(path)
     val result =
       objectUnderTest.write(path.toString, content).unsafeRunSync()
     //then
     result shouldBe Right(())
     Source.fromFile(path.toFile).getLines().mkString shouldBe content
+  }
+
+  it should "overwrite existing files" in new TestCtx {
+    //given
+    val path            = Paths.get(TestDirPath.toString, "foo.txt")
+    val existingContent = "123456789"
+    val newContent      = "abcdef"
+    //when
+    objectUnderTest.write(path.toString, existingContent).unsafeRunSync()
+    objectUnderTest.write(path.toString, newContent).unsafeRunSync()
+    //then
+    Source.fromFile(path.toFile).getLines().mkString shouldBe newContent
   }
 
   trait TestCtx {
