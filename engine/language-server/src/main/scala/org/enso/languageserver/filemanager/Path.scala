9be446fe57948d59654a538335366ac71a2fb264
lolczak <lukasz.jan.olczak@gmail.com>
2020-02-24 17:33:16 +0100

    scala doc

--- engine/language-server/src/main/scala/org/enso/languageserver/filemanager/Path.scala
+++ engine/language-server/src/main/scala/org/enso/languageserver/filemanager/Path.scala
@@ -2,4 +2,10 @@ package org.enso.languageserver.filemanager
 
 import java.util.UUID
 
+/**
+  * A representation of a path relative to a specified content root.
+  *
+  * @param rootId a content root id that the path is relative to
+  * @param segments path segments
+  */
 case class Path(rootId: UUID, segments: List[String])
