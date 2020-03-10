14e0d8296e61b16613c5579b97554f1337920dcb
lolczak <lukasz.jan.olczak@gmail.com>
2020-03-10 11:25:00 +0100

    tests

--- engine/language-server/src/test/scala/org/enso/languageserver/text/RopeTextEditorSpec.scala
+++ engine/language-server/src/test/scala/org/enso/languageserver/text/RopeTextEditorSpec.scala
@@ -10,13 +10,30 @@ class RopeTextEditorSpec extends AnyFlatSpec with Matchers {
 
   "A rope text editor" should "insert text before beginning of a line" in {
     //given
-    val beforeMain       = Range(Position(1, 0), Position(1, 0))
-    val insertBeforeMain = TextEdit(beforeMain, "ultra_")
+    val beforeMain          = Range(Position(1, 0), Position(1, 0))
+    val insertionBeforeMain = TextEdit(beforeMain, "ultra_")
     //when
-    val result = RopeTextEditor.edit(rope, insertBeforeMain)
+    val result = RopeTextEditor.edit(rope, insertionBeforeMain)
     //then
     result.toString mustBe """
                              |ultra_main =
+                             |    apply = v f -> f v
+                             |    adder = a b -> a + b
+                             |    plusOne = apply (f = adder 1)
+                             |    result = plusOne 10
+                             |    result
+                             |""".stripMargin
+  }
+
+  it should "replace a substring" in {
+    //given
+    val mainPosition    = Range(Position(1, 0), Position(1, 4))
+    val mainReplacement = TextEdit(mainPosition, "run")
+    //when
+    val result = RopeTextEditor.edit(rope, mainReplacement)
+    //then
+    result.toString mustBe """
+                             |run =
                              |    apply = v f -> f v
                              |    adder = a b -> a + b
                              |    plusOne = apply (f = adder 1)
