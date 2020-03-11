c80559eaded7fbaca10ceb6cf69cd446f4f5e363
lolczak <lukasz.jan.olczak@gmail.com>
2020-03-11 10:22:03 +0100

    refactoring

--- engine/language-server/src/main/scala/org/enso/languageserver/text/editing/EditorOps.scala
+++ engine/language-server/src/main/scala/org/enso/languageserver/text/editing/EditorOps.scala
@@ -1,20 +1,23 @@
 package org.enso.languageserver.text.editing
 
+import cats.implicits._
+import org.enso.languageserver.text.editing.TextEditValidator.validate
 import org.enso.languageserver.text.editing.model.TextEdit
 
 object EditorOps {
+
+  def edit[A: TextEditor](
+    buffer: A,
+    diff: TextEdit
+  ): Either[TextEditValidationFailure, A] =
+    validate(buffer, diff).map(_ => TextEditor[A].edit(buffer, diff))
 
   def applyEdits[A: TextEditor](
     buffer: A,
     diffs: Seq[TextEdit]
   ): Either[TextEditValidationFailure, A] =
     diffs.foldLeft[Either[TextEditValidationFailure, A]](Right(buffer)) {
-      case (maybeBuffer, diff) =>
-        for {
-          currentBuffer <- maybeBuffer
-          _             <- TextEditValidator.validate(currentBuffer, diff)
-          newBuffer = TextEditor[A].edit(currentBuffer, diff)
-        } yield newBuffer
+      case (maybeBuffer, diff) => maybeBuffer >>= (edit(_, diff))
     }
 
 }
