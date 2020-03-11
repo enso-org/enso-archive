730951747dcbef68bc163d434a786f97ed39e960
lolczak <lukasz.jan.olczak@gmail.com>
2020-03-11 14:08:33 +0100

    refactoring

--- engine/language-server/src/test/scala/org/enso/languageserver/websocket/TextOperationsTest.scala
+++ engine/language-server/src/test/scala/org/enso/languageserver/websocket/TextOperationsTest.scala
@@ -1461,27 +1461,15 @@ class TextOperationsTest extends WebSocketServerTest {
                   "edits" : [
                     {
                       "range" : {
-                        "start" : {
-                          "line" : 0,
-                          "character" : 0
-                        },
-                        "end" : {
-                          "line" : 0,
-                          "character" : 0
-                        }
+                        "start" : { "line" : 0, "character" : 0 },
+                        "end" : { "line" : 0, "character" : 0 }
                       },
                       "text" : "bar"
                     },
                     {
                       "range" : {
-                        "start" : {
-                          "line" : 0,
-                          "character" : 12
-                        },
-                        "end" : {
-                          "line" : 0,
-                          "character" : 12
-                        }
+                        "start" : { "line" : 0, "character" : 12 },
+                        "end" : { "line" : 0, "character" : 12 }
                       },
                       "text" : "foo"
                     }
