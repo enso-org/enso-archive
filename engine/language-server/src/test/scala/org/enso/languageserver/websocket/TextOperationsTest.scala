e276d612a8c35cbb3a89b672905ccf178ee90589
lolczak <lukasz.jan.olczak@gmail.com>
2020-03-11 14:05:59 +0100

    tests

--- engine/language-server/src/test/scala/org/enso/languageserver/websocket/TextOperationsTest.scala
+++ engine/language-server/src/test/scala/org/enso/languageserver/websocket/TextOperationsTest.scala
@@ -1326,6 +1326,175 @@ class TextOperationsTest extends WebSocketServerTest {
           """)
     }
 
+    "should notify subscribers about a change" in {
+      val client1 = new WsTestClient(address)
+      val client2 = new WsTestClient(address)
+
+      client1.send(json"""
+          { "jsonrpc": "2.0",
+            "method": "file/write",
+            "id": 0,
+            "params": {
+              "path": {
+                "rootId": $testContentRootId,
+                "segments": [ "text-edit-2.txt" ]
+              },
+              "contents": "123456789"
+            }
+          }
+          """)
+      client1.expectJson(json"""
+          { "jsonrpc": "2.0",
+            "id": 0,
+            "result": null
+          }
+          """)
+      client1.send(json"""
+          { "jsonrpc": "2.0",
+            "method": "text/openFile",
+            "id": 1,
+            "params": {
+              "path": {
+                "rootId": $testContentRootId,
+                "segments": [ "text-edit-2.txt" ]
+              }
+            }
+          }
+          """)
+      client1.expectJson(json"""
+          {
+            "jsonrpc" : "2.0",
+            "id" : 1,
+            "result" : {
+              "writeCapability" : {
+                "method" : "canEdit",
+                "registerOptions" : {
+                  "path" : {
+                    "rootId" : $testContentRootId,
+                    "segments" : [
+                      "text-edit-2.txt"
+                    ]
+                  }
+                }
+              },
+              "content" : "123456789",
+              "currentVersion" : "5795c3d628fd638c9835a4c79a55809f265068c88729a1a3fcdf8522"
+            }
+          }
+          """)
+
+      client2.send(json"""
+          { "jsonrpc": "2.0",
+            "method": "text/openFile",
+            "id": 1,
+            "params": {
+              "path": {
+                "rootId": $testContentRootId,
+                "segments": [ "text-edit-2.txt" ]
+              }
+            }
+          }
+          """)
+      client2.expectJson(json"""
+          {
+            "jsonrpc" : "2.0",
+            "id" : 1,
+            "result" : {
+              "writeCapability" : null,
+              "content" : "123456789",
+              "currentVersion" : "5795c3d628fd638c9835a4c79a55809f265068c88729a1a3fcdf8522"
+            }
+          }
+          """)
+
+      client1.send(json"""
+          { "jsonrpc": "2.0",
+            "method": "text/applyEdit",
+            "id": 2,
+            "params": {
+              "edit": {
+                "path": {
+                  "rootId": $testContentRootId,
+                  "segments": [ "text-edit-2.txt" ]
+                },
+                "oldVersion": "5795c3d628fd638c9835a4c79a55809f265068c88729a1a3fcdf8522",
+                "newVersion": "ebe55342f9c8b86857402797dd723fb4a2174e0b56d6ace0a6929ec3",
+                "edits": [
+                  {
+                    "range": {
+                      "start": { "line": 0, "character": 0 },
+                      "end": { "line": 0, "character": 0 }
+                    },
+                    "text": "bar"
+                  },
+                  {
+                    "range": {
+                      "start": { "line": 0, "character": 12 },
+                      "end": { "line": 0, "character": 12 }
+                    },
+                    "text": "foo"
+                  }
+                ]
+              }
+            }
+          }
+          """)
+      client1.expectJson(json"""
+          { "jsonrpc": "2.0",
+            "id": 2,
+            "result": null
+          }
+          """)
+      client2.expectJson(json"""
+          {
+            "jsonrpc" : "2.0",
+            "method" : "text/didChange",
+            "params" : {
+              "edits" : [
+                {
+                  "path" : {
+                    "rootId" : $testContentRootId,
+                    "segments" : [
+                      "text-edit-2.txt"
+                    ]
+                  },
+                  "edits" : [
+                    {
+                      "range" : {
+                        "start" : {
+                          "line" : 0,
+                          "character" : 0
+                        },
+                        "end" : {
+                          "line" : 0,
+                          "character" : 0
+                        }
+                      },
+                      "text" : "bar"
+                    },
+                    {
+                      "range" : {
+                        "start" : {
+                          "line" : 0,
+                          "character" : 12
+                        },
+                        "end" : {
+                          "line" : 0,
+                          "character" : 12
+                        }
+                      },
+                      "text" : "foo"
+                    }
+                  ],
+                  "oldVersion" : "5795c3d628fd638c9835a4c79a55809f265068c88729a1a3fcdf8522",
+                  "newVersion" : "ebe55342f9c8b86857402797dd723fb4a2174e0b56d6ace0a6929ec3"
+                }
+              ]
+            }
+          }
+          """)
+    }
+
   }
 
 }
