453823988c73d9d98ab0aa29d1ce9c53f0c0f08b
lolczak <lukasz.jan.olczak@gmail.com>
2020-03-12 10:48:23 +0100

    tests

--- engine/language-server/src/test/scala/org/enso/languageserver/websocket/TextOperationsTest.scala
+++ engine/language-server/src/test/scala/org/enso/languageserver/websocket/TextOperationsTest.scala
@@ -1549,8 +1549,87 @@ class TextOperationsTest extends WebSocketServerTest {
           """)
     }
 
-    "fail when a client's version doesn't match a server version" in {}
-    "fail when a client doesn't hold a write lock" in                {}
+    "fail when a client's version doesn't match a server version" in {
+      val client = new WsTestClient(address)
+
+      client.send(json"""
+          { "jsonrpc": "2.0",
+            "method": "file/write",
+            "id": 0,
+            "params": {
+              "path": {
+                "rootId": $testContentRootId,
+                "segments": [ "foo.txt" ]
+              },
+              "contents": "123456789"
+            }
+          }
+          """)
+      client.expectJson(json"""
+          { "jsonrpc": "2.0",
+            "id": 0,
+            "result": null
+          }
+          """)
+      client.send(json"""
+          { "jsonrpc": "2.0",
+            "method": "text/openFile",
+            "id": 1,
+            "params": {
+              "path": {
+                "rootId": $testContentRootId,
+                "segments": [ "foo.txt" ]
+              }
+            }
+          }
+          """)
+      client.expectJson(json"""
+          {
+            "jsonrpc" : "2.0",
+            "id" : 1,
+            "result" : {
+              "writeCapability" : {
+                "method" : "canEdit",
+                "registerOptions" : {
+                  "path" : {
+                    "rootId" : $testContentRootId,
+                    "segments" : [
+                      "foo.txt"
+                    ]
+                  }
+                }
+              },
+              "content" : "123456789",
+              "currentVersion" : "5795c3d628fd638c9835a4c79a55809f265068c88729a1a3fcdf8522"
+            }
+          }
+          """)
+      client.send(json"""
+          { "jsonrpc": "2.0",
+            "method": "text/save",
+            "id": 3,
+            "params": {
+              "path": {
+                "rootId": $testContentRootId,
+                "segments": [ "foo.txt" ]
+              },
+              "currentVersion": "ebe55342f9c8b86857402797dd723fb4a2174e0b56d6ace0a6929ec3"
+            }
+          }
+          """)
+      client.expectJson(json"""
+          { "jsonrpc": "2.0",
+            "id": 3,
+            "error": { 
+              "code": 3003, 
+              "message": "Invalid version [client version: ebe55342f9c8b86857402797dd723fb4a2174e0b56d6ace0a6929ec3, server version: 5795c3d628fd638c9835a4c79a55809f265068c88729a1a3fcdf8522]" 
+            }
+          }
+          """)
+    }
+
+    "fail when a client doesn't hold a write lock" in {}
+
     "persist changes from a buffer to durable storage" in {
       val client = new WsTestClient(address)
 
