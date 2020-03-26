77709443d676b4ed224302c8b4a8d92186b16e0a
Dmitry Bushev <bushevdv@gmail.com>
2020-03-26 08:38:13 +0300

    test: DEBUG macOS

--- engine/language-server/src/test/scala/org/enso/languageserver/filemanager/FileEventWatcherSpec.scala
+++ engine/language-server/src/test/scala/org/enso/languageserver/filemanager/FileEventWatcherSpec.scala
@@ -17,48 +17,58 @@ class FileEventWatcherSpec extends AnyFlatSpec with Matchers {
   final val Timeout: FiniteDuration = 5.seconds
 
   it should "get create events" in withWatcher { (path, events) =>
-    val fileA = Paths.get(path.toString, "a.txt")
+    val fileA = Paths.get(path.toString, "1.txt")
 
     Files.createFile(fileA)
-    val event = events.poll(Timeout.length, Timeout.unit)
-    event shouldBe WatcherEvent(fileA, EventTypeCreate)
+    // val event = events.poll(Timeout.length, Timeout.unit)
+    // event shouldBe WatcherEvent(fileA, EventTypeCreate)
+    consume(events)
   }
 
   it should "get delete events" in withWatcher { (path, events) =>
-    val fileA = Paths.get(path.toString, "a.txt")
+    val fileA = Paths.get(path.toString, "2.txt")
 
     Files.createFile(fileA)
-    val event1 = events.poll(Timeout.length, Timeout.unit)
-    event1 shouldBe WatcherEvent(fileA, EventTypeCreate)
+    // val event1 = events.poll(Timeout.length, Timeout.unit)
+    // event1 shouldBe WatcherEvent(fileA, EventTypeCreate)
 
     Files.delete(fileA)
-    val event2 = events.poll(Timeout.length, Timeout.unit)
-    event2 shouldBe WatcherEvent(fileA, EventTypeDelete)
+    // val event2 = events.poll(Timeout.length, Timeout.unit)
+    // event2 shouldBe WatcherEvent(fileA, EventTypeDelete)
+    consume(events)
   }
 
   it should "get modify events" in withWatcher { (path, events) =>
-    val fileA = Paths.get(path.toString, "a.txt")
+    val fileA = Paths.get(path.toString, "3.txt")
 
     Files.createFile(fileA)
-    val event1 = events.poll(Timeout.length, Timeout.unit)
-    event1 shouldBe WatcherEvent(fileA, EventTypeCreate)
+    // val event1 = events.poll(Timeout.length, Timeout.unit)
+    // event1 shouldBe WatcherEvent(fileA, EventTypeCreate)
 
     Files.write(fileA, "hello".getBytes())
-    val event2 = events.poll(Timeout.length, Timeout.unit)
-    event2 shouldBe WatcherEvent(fileA, EventTypeModify)
+    // val event2 = events.poll(Timeout.length, Timeout.unit)
+    // event2 shouldBe WatcherEvent(fileA, EventTypeModify)
+    consume(events)
   }
 
   it should "get events from subdirectories" in withWatcher { (path, events) =>
     val subdir = Paths.get(path.toString, "subdir")
-    val fileA  = Paths.get(path.toString, "subdir", "a.txt")
+    val fileA  = Paths.get(path.toString, "subdir", "4.txt")
 
     Files.createDirectories(subdir)
-    val event1 = events.poll(Timeout.length, Timeout.unit)
-    event1 shouldBe WatcherEvent(subdir, EventTypeCreate)
+    // val event1 = events.poll(Timeout.length, Timeout.unit)
+    // event1 shouldBe WatcherEvent(subdir, EventTypeCreate)
 
     Files.createFile(fileA)
-    val event2 = events.poll(Timeout.length, Timeout.unit)
-    event2 shouldBe WatcherEvent(fileA, EventTypeCreate)
+    // val event2 = events.poll(Timeout.length, Timeout.unit)
+    // event2 shouldBe WatcherEvent(fileA, EventTypeCreate)
+    consume(events)
+  }
+
+  it should "test macOS file events" in withWatcherMacOs { (path, events) =>
+    val file = Paths.get(path.toString, "5.txt")
+    Files.createFile(file)
+    consume(events)
   }
 
   def withWatcher(
@@ -79,6 +89,38 @@ class FileEventWatcherSpec extends AnyFlatSpec with Matchers {
       executor.shutdown()
       Try(executor.awaitTermination(Timeout.length, Timeout.unit))
       Try(FileUtils.deleteDirectory(tmp.toFile)): Unit
+    }
+  }
+
+  def withWatcherMacOs(
+    test: (Path, LinkedBlockingQueue[WatcherEvent]) => Any
+  ): Any = {
+    val executor = Executors.newSingleThreadExecutor()
+    val tmp      = Files.createTempDirectory(null)
+    val keep     = Paths.get(tmp.toString, ".keep")
+    Files.createFile(keep)
+    val queue    = new LinkedBlockingQueue[WatcherEvent]()
+    val watcher  = new FileEventWatcher(tmp, queue.put(_))
+
+    executor.submit(new Runnable {
+      def run() = watcher.start()
+    })
+
+    try test(tmp, queue)
+    finally {
+      watcher.stop()
+      executor.shutdown()
+      Try(executor.awaitTermination(Timeout.length, Timeout.unit))
+      Try(FileUtils.deleteDirectory(tmp.toFile)): Unit
+    }
+  }
+
+  // poll until the timeout exception is thrown
+  def consume[A <: AnyRef](queue: LinkedBlockingQueue[A]): Unit = {
+    var elem = queue.poll(Timeout.length, Timeout.unit)
+    while (elem ne null) {
+      println(elem)
+      elem = queue.poll(Timeout.length, Timeout.unit)
     }
   }
 }
