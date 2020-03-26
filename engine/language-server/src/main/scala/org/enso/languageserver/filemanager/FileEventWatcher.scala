5f3ab2716791fb33a33b4ff64c7bfe8f907ea1ad
Dmitry Bushev <bushevdv@gmail.com>
2020-03-26 18:12:23 +0300

    test: DEBUG macOS

--- engine/language-server/src/main/scala/org/enso/languageserver/filemanager/FileEventWatcher.scala
+++ engine/language-server/src/main/scala/org/enso/languageserver/filemanager/FileEventWatcher.scala
@@ -19,7 +19,7 @@ final class FileEventWatcher(
 
   private val watcher: DirectoryWatcher = DirectoryWatcher
     .builder()
-    .path(root)
+    .path(root.toRealPath())
     .listener(this)
     .build()
 
