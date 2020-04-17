797cee949d049514240c7ae65e215a714cf8cdf1
lolczak <lukasz.jan.olczak@gmail.com>
2020-04-17 13:49:23 +0200

    plugin adjusted to spec, now generates classes to sourceManaged dir

--- project/GenerateFlatbuffers.scala
+++ project/GenerateFlatbuffers.scala
@@ -1,45 +1,35 @@
 import sbt.Keys._
 import sbt._
+
 import scala.sys.process._
 
 object GenerateFlatbuffers extends AutoPlugin {
   override def requires = sbt.plugins.JvmPlugin
   override def trigger  = allRequirements
 
-  object autoImport {
-    val generateFlatbuffers =
-      taskKey[Unit]("Run flatc compiler to generate Java classes for schema")
-  }
-  import autoImport._
+  lazy val generateFlatbuffersTask = Def.task {
+    val flatcCmd = "flatc"
+    val root     = baseDirectory.value
+    val schemas =
+      (file(s"$root/src/main/schema") ** "*.fbs").get
+        .map(_.getAbsolutePath)
+        .toList
 
-  lazy val generateFlatbuffersTask = generateFlatbuffers := {
-      val flatcCmd = "flatc"
-//        System.getProperty("os.name").toLowerCase match {
-//          case mac if mac.contains("mac")       => "flatc"
-//          case win if win.contains("win")       => "flatc.exe"
-//          case linux if linux.contains("linux") => "flatc"
-//          case osName =>
-//            throw new RuntimeException(s"Unknown operating system $osName")
-//        }
-      val root = baseDirectory.value
-      val schemas =
-        (file(s"$root/src/main/schema") ** "*.fbs").get
-          .map(_.getAbsolutePath)
-          .toList
+    val out = (sourceManaged in Compile).value
 
-      schemas foreach { schema =>
-        println(s"*** Generating Java classes for schema: $schema")
-        val result = s"$flatcCmd --java -o $root/src/main/java $schema".!!
-        println(
-          s"*** Generated Java classes from FlatBuffer schema $schema. Results: '$result'"
-        )
-      }
+    schemas foreach { schema =>
+      val cmd = s"$flatcCmd --java -o ${out.getAbsolutePath} $schema"
+      println(s"*** Generating Java classes for schema: $schema, cmd: $cmd")
+      val result = cmd.!!
+      println(
+        s"*** Generated Java classes from FlatBuffer schema $schema. Results: $result"
+      )
     }
+    (out ** "*.java").get
+  }
 
   override lazy val projectSettings = Seq(
-    generateFlatbuffersTask,
-    (compile in Compile) := (compile in Compile dependsOn generateFlatbuffers).value,
-    (compile in Test) := (compile in Test dependsOn generateFlatbuffers).value
+    sourceGenerators in Compile += generateFlatbuffersTask
   )
 
 }
