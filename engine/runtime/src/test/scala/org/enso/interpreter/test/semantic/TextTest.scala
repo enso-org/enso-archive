1ab18fa6d81b384e0915450e77e7f037857096b5
Marcin Kostrzewa <marckostrzewa@gmail.com>
2020-04-21 15:37:09 +0200

    abort mission

--- engine/runtime/src/test/scala/org/enso/interpreter/test/semantic/TextTest.scala
+++ engine/runtime/src/test/scala/org/enso/interpreter/test/semantic/TextTest.scala
@@ -13,29 +13,30 @@ class TextTest extends InterpreterTest {
     consumeOut shouldEqual List("hello world!")
   }
 
-  "Single line interpolated literals" should "exist in the language" in {
+  "String concatenation" should "exist in the language" in {
     val code =
       """
         |main =
-        |    w = "World"
-        |    IO.println 'Hello, `w`!'
+        |    h = "Hello, "
+        |    w = "World!"
+        |    IO.println h+w
         |""".stripMargin
     eval(code)
     consumeOut shouldEqual List("Hello, World!")
   }
 
-  "Interpolated literals" should "delegate to the `to_text` method on the expression result" in {
+  "Arbitrary structures" should "be auto-convertible to strings with the to_text method" in {
     val code =
       """
         |type My_Type a
         |
-        |My_Type.to_text = case this of
-        |    My_Type a -> 'SurpriseString::`a`'
-        |
-        |main = IO.println 'Hello, `My_Type 10`'
+        |main =
+        |    IO.println 5.to_text
+        |    IO.println (My_Type (My_Type 10)).to_text
+        |    IO.println "123".to_text
         |""".stripMargin
     eval(code)
-    consumeOut shouldEqual List("Hello, SurpriseString::10")
+    consumeOut shouldEqual List("5", "My_Type (My_Type 10)", "123")
   }
 
   "Block raw text literals" should "exist in the language" in {
