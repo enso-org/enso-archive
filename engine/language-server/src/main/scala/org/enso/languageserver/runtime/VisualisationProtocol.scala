c456cd53de0b824e1277d46db36514a5cf1a1033
lolczak <lukasz.jan.olczak@gmail.com>
2020-04-22 15:21:05 +0200

    scala docs

--- engine/language-server/src/main/scala/org/enso/languageserver/runtime/VisualisationProtocol.scala
+++ engine/language-server/src/main/scala/org/enso/languageserver/runtime/VisualisationProtocol.scala
@@ -4,12 +4,25 @@ import java.util.UUID
 
 object VisualisationProtocol {
 
+  /**
+    * Represents a visualisation context.
+    *
+    * @param visualisationId a visualisation identifier
+    * @param contextId a context identifier
+    * @param expressionId an expression identifier
+    */
   case class VisualisationContext(
     visualisationId: UUID,
     contextId: UUID,
     expressionId: UUID
   )
 
+  /**
+    * Event signaling visualisation update.
+    *
+    * @param visualisationContext a visualisation context
+    * @param data a visualisation data
+    */
   case class VisualisationUpdate(
     visualisationContext: VisualisationContext,
     data: Array[Byte]
