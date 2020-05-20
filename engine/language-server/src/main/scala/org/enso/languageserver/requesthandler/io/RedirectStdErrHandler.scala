e70a4e812c80dcd165ee98d134ba52775366cefc
lolczak <lukasz.jan.olczak@gmail.com>
2020-05-20 14:12:36 +0200

    scala docs

--- engine/language-server/src/main/scala/org/enso/languageserver/requesthandler/io/RedirectStdErrHandler.scala
+++ engine/language-server/src/main/scala/org/enso/languageserver/requesthandler/io/RedirectStdErrHandler.scala
@@ -7,6 +7,12 @@ import org.enso.languageserver.io.InputOutputApi.RedirectStandardError
 import org.enso.languageserver.io.InputOutputProtocol
 import org.enso.languageserver.util.UnhandledLogging
 
+/**
+  * A request handler for `io/redirectStandardError` commands.
+  *
+  * @param stdErrController an output redirection controller
+  * @param clientId a client requesting redirection
+  */
 class RedirectStdErrHandler(stdErrController: ActorRef, clientId: ClientId)
     extends Actor
     with ActorLogging
@@ -23,6 +29,13 @@ class RedirectStdErrHandler(stdErrController: ActorRef, clientId: ClientId)
 
 object RedirectStdErrHandler {
 
+  /**
+    * Creates a configuration object used to create a [[RedirectStdErrHandler]].
+    *
+    * @param stdErrController an output redirection controller
+    * @param clientId a client requesting redirection
+    * @return a configuration object
+    */
   def props(stdErrController: ActorRef, clientId: ClientId): Props =
     Props(new RedirectStdErrHandler(stdErrController, clientId))
 
--- engine/language-server/src/main/scala/org/enso/languageserver/requesthandler/io/RedirectStdOutHandler.scala
+++ engine/language-server/src/main/scala/org/enso/languageserver/requesthandler/io/RedirectStdOutHandler.scala
@@ -7,6 +7,12 @@ import org.enso.languageserver.io.InputOutputApi.RedirectStandardOutput
 import org.enso.languageserver.io.InputOutputProtocol
 import org.enso.languageserver.util.UnhandledLogging
 
+/**
+  * A request handler for `io/redirectStandardOutput` commands.
+  *
+  * @param stdOutController an output redirection controller
+  * @param clientId a client requesting redirection
+  */
 class RedirectStdOutHandler(stdOutController: ActorRef, clientId: ClientId)
     extends Actor
     with ActorLogging
@@ -23,6 +29,13 @@ class RedirectStdOutHandler(stdOutController: ActorRef, clientId: ClientId)
 
 object RedirectStdOutHandler {
 
+  /**
+    * Creates a configuration object used to create a [[RedirectStdOutHandler]].
+    *
+    * @param stdOutController an output redirection controller
+    * @param clientId a client requesting redirection
+    * @return a configuration object
+    */
   def props(stdOutController: ActorRef, clientId: ClientId): Props =
     Props(new RedirectStdOutHandler(stdOutController, clientId))
 
