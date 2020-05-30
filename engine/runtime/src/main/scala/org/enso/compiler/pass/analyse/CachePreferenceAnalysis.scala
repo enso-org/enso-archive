9ad0d1d700441be8a30267aaba653abb8e73c6df
Dmitry Bushev <bushevdv@gmail.com>
2020-05-30 18:01:29 +0300

    update: simplify cache preference pass

--- engine/runtime/src/main/scala/org/enso/compiler/pass/analyse/CachePreferenceAnalysis.scala
+++ engine/runtime/src/main/scala/org/enso/compiler/pass/analyse/CachePreferenceAnalysis.scala
@@ -131,138 +131,23 @@ case object CachePreferenceAnalysis extends IRPass {
     expression: IR.Expression,
     weights: WeightInfo
   ): IR.Expression = {
-    expression match {
-      case empty: IR.Empty       => empty.updateMetadata(this -->> weights)
-      case function: IR.Function => analyseFunction(function, weights)
-      case app: IR.Application   => analyseApplication(app, weights)
-      case typ: IR.Type          => typ.updateMetadata(this -->> weights)
-      case name: IR.Name         => name.updateMetadata(this -->> weights)
-      case cse: IR.Case          => analyseCase(cse, weights)
-      case _: IR.Comment =>
-        throw new CompilerError(
-          "Comments should not be present during cache preference analysis."
-        )
-      case literal: IR.Literal =>
-        literal.updateMetadata(this -->> weights)
-      case foreign: IR.Foreign =>
-        foreign.updateMetadata(this -->> weights)
-
-      case block @ IR.Expression.Block(expressions, returnValue, _, _, _, _) =>
-        block
-          .copy(
-            expressions = expressions.map(analyseExpression(_, weights)),
-            returnValue = analyseExpression(returnValue, weights)
-          )
-          .updateMetadata(this -->> weights)
-
-      case binding @ IR.Expression.Binding(name, expression, _, _, _) =>
-        expression.getExternalId.foreach(weights.update(_, Weight.Always))
+    expression.transformExpressions {
+      case binding: IR.Expression.Binding =>
+        binding.expression.getExternalId
+          .foreach(weights.update(_, Weight.Always))
         binding
           .copy(
-            name       = name.updateMetadata(this -->> weights),
-            expression = analyseExpression(expression, weights)
+            name       = binding.name.updateMetadata(this -->> weights),
+            expression = analyseExpression(binding.expression, weights)
           )
           .updateMetadata(this -->> weights)
-
-      case error: IR.Error => error
-    }
-  }
-
-  /** Performs preference analysis on a function.
-    *
-    * @param function the function to perform preference analysis on
-    * @param weights the weights information for the module
-    * @return `function`, with attached preference information
-    */
-  def analyseFunction(
-    function: IR.Function,
-    weights: WeightInfo
-  ): IR.Function = {
-    function match {
-      case lam @ IR.Function.Lambda(arguments, body, _, _, _, _) =>
-        lam
-          .copy(
-            arguments = arguments.map(analyseDefinitionArgument(_, weights)),
-            body      = analyseExpression(body, weights)
-          )
-          .updateMetadata(this -->> weights)
-      case _: IR.Function.Binding =>
-        throw new CompilerError(
-          "Function sugar should not be present during cache preference analysis."
-        )
-    }
-  }
-
-  /** Preforms preference analysis on an application.
-    *
-    * @param application the appliation to perform preference analysis on
-    * @param weights the weights information for the module
-    * @return `application`, with attached preference information
-    */
-  def analyseApplication(
-    application: IR.Application,
-    weights: WeightInfo
-  ): IR.Application = {
-    application match {
-      case prefix @ IR.Application.Prefix(fn, args, _, _, _, _) =>
-        prefix
-          .copy(
-            function  = analyseExpression(fn, weights),
-            arguments = args.map(analyseCallArgument(_, weights))
-          )
-          .updateMetadata(this -->> weights)
-      case force @ IR.Application.Force(target, _, _, _) =>
-        force
-          .copy(target = analyseExpression(target, weights))
-          .updateMetadata(this -->> weights)
-      case vector @ IR.Application.Literal.Sequence(items, _, _, _) =>
-        vector
-          .copy(items = items.map(analyseExpression(_, weights)))
-          .updateMetadata(this -->> weights)
-      case _: IR.Application.Operator =>
-        throw new CompilerError(
-          "Unexpected operator during cache preference analysis."
-        )
-    }
-  }
-
-  /** Preforms preference analysis on a case expression.
-    *
-    * @param cse the case expression to perform preference analysis on
-    * @param weights the weights information for the module
-    * @return `cse`, with attached preference information
-    */
-  def analyseCase(cse: IR.Case, weights: WeightInfo): IR.Case = {
-    cse match {
-      case expr @ IR.Case.Expr(scrutinee, branches, fallback, _, _, _) =>
+      case error: IR.Error =>
+        error
+      case expr =>
         expr
-          .copy(
-            scrutinee = analyseExpression(scrutinee, weights),
-            branches  = branches.map(analyseCaseBranch(_, weights)),
-            fallback  = fallback.map(analyseExpression(_, weights))
-          )
+          .mapExpressions(analyseExpression(_, weights))
           .updateMetadata(this -->> weights)
-      case _: IR.Case.Branch =>
-        throw new CompilerError("Unexpected case branch.")
     }
-  }
-
-  /** Preforms preference analysis on a case branch.
-    *
-    * @param branch the case branch to perform preference analysis on
-    * @param weights the weights information for the module
-    * @return `branch`, with attached preference information
-    */
-  def analyseCaseBranch(
-    branch: IR.Case.Branch,
-    weights: WeightInfo
-  ): IR.Case.Branch = {
-    branch
-      .copy(
-        pattern    = analyseExpression(branch.pattern, weights),
-        expression = analyseExpression(branch.expression, weights)
-      )
-      .updateMetadata(this -->> weights)
   }
 
   /** Performs preference analysis on a function definition argument.
@@ -279,24 +164,6 @@ case object CachePreferenceAnalysis extends IRPass {
       case spec @ IR.DefinitionArgument.Specified(_, defValue, _, _, _, _) =>
         spec
           .copy(defaultValue = defValue.map(analyseExpression(_, weights)))
-          .updateMetadata(this -->> weights)
-    }
-  }
-
-  /** Performs preference analysis on a function call argument.
-    *
-    * @param argument the call argument to perform the analysis on
-    * @param weights the weights information for the module
-    * @return `argument`, with attached preference information
-    */
-  def analyseCallArgument(
-    argument: IR.CallArgument,
-    weights: WeightInfo
-  ): IR.CallArgument = {
-    argument match {
-      case spec @ IR.CallArgument.Specified(_, value, _, _, _, _) =>
-        spec
-          .copy(value = analyseExpression(value, weights))
           .updateMetadata(this -->> weights)
     }
   }
